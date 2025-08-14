2b76c3e59323a3e8553013633a913deb135eeb68
김도훈 <kde6571@gmail.com>
2025-08-14 10:54:55 +0900

    SAME: SWEA 2806(D3) 백준 9663과 동일, 20250813 백트래킹 실습 1번 해결

---
+++ 김도훈/src/com/kdedevelop/swea/P2806/Solution.java
@@ -0,0 +1,61 @@
+package com.kdedevelop.swea.P2806;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Solution {
+	public static int result;
+	
+	public static boolean isCanPlace(int place, int[] placed, int depth) {
+		boolean result = true;
+		for (int i = 0 ; i < depth ; i ++) {
+			if (placed[i] == place) result = false;
+			int depthDiff = depth - i;
+			int diff = placed[i] - place;
+			if (diff < 0) diff *= -1;
+			if (diff == depthDiff) result = false;
+		}
+		
+//		System.out.println("PLACE : " + place + " || PLACED : " + Arrays.toString(placed) + " || DEPTH : " + depth + " || RESULT : " + result);
+		return result;
+	}
+	
+	public static void function(int N, int depth, int[] placed) {
+		if (depth == N) {
+//			System.out.println(Arrays.toString(placed));
+			result ++;
+		} else {
+			for (int j = 0 ; j < N ; j ++) {
+				if (isCanPlace(j, placed, depth)) {
+					placed[depth] = j;
+					function(N, depth+1, placed);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int TotalTestCase = Integer.parseInt(br.readLine());
+		
+		for (int testCase = 0 ; testCase < TotalTestCase ; testCase ++) {
+			result = 0;
+			
+			int N = Integer.parseInt(br.readLine());
+			
+			function(N, 0, new int[N]);
+			
+			bw.write("#" + (testCase+1) + " " + String.valueOf(result) + "\n");
+			
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
